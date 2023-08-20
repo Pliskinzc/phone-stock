497ffd6fdfcb06156ae3d2bee9601c6a8d93bf0f
Pliskinzc <pliskinzc@gmail.com>
2023-08-20 14:46:54 +0200

    aws config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+
 const app = createApp(App)
 
 app.use(createPinia())
